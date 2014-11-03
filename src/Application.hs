0f525060177b91c3719559dbaa36fcc73b32e454
Jacob Stanley <jacob@stanley.io>
2014-11-04 02:15:50 +1100

    Got flow-ui compiling even when linked to limp-cbc

--- src/Application.hs
+++ src/Application.hs
@@ -1,31 +1,30 @@
-{-# LANGUAGE TemplateHaskell #-}
-
-------------------------------------------------------------------------------
--- | This module defines our application's state type and an alias for its
--- handler monad.
 module Application where
 
-------------------------------------------------------------------------------
+import Control.Applicative ((<$>))
 import Control.Lens
 import Snap.Snaplet
 import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
 import Snap.Snaplet.Session
 
 ------------------------------------------------------------------------------
+
 data App = App
     { _heist :: Snaplet (Heist App)
     , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
     }
 
-makeLenses ''App
+-- Can't use TemplateHaskell with limp-cbc :(
+-- $(makeLenses ''App)
+
+heist :: Lens' App (Snaplet (Heist App))
+heist inj (App h s) = (\h' -> App h' s) <$> inj h
+{-# INLINE heist #-}
+
+sess :: Lens' App (Snaplet SessionManager)
+sess inj (App h s) = (\s' -> App h s') <$> inj s
+{-# INLINE sess #-}
 
 instance HasHeist App where
     heistLens = subSnaplet heist
 
-
-------------------------------------------------------------------------------
 type AppHandler = Handler App App
-
-
